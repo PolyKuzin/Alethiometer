909d3e57f111ddf063483014dba43209d7d33e4a
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-11-22 18:58:50 +0300

    Initial Commit

---
+++ Gliese_581/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gliese_581
+//
+//  Created by Павел Кузин on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
